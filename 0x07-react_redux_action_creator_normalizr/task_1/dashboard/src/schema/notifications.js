a72be01e3897395b2b0795bf00156ebcbc887989
Makoena20 <koenaletseku@gmail.com>
2024-10-01 09:57:55 +0000

    task_1

---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,22 @@
+// Import necessary modules from Normalizr
+import { normalize, schema } from 'normalizr';
+import * as notificationsData from '../../notifications.json';
+
+// Define the user entity
+const user = new schema.Entity('users');
+
+// Define the message entity
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+// Define the notification entity
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+// Normalize the notifications data
+export const normalizedData = normalize(notificationsData.default, [notification]);
+
+// Export the normalized data
+export default normalizedData;
+
