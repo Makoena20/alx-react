6bddf7b416c8b03875703d4afb101714dcd9935a
Makoena20 <koenaletseku@gmail.com>
2024-10-02 13:14:02 +0000

    task_5

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,18 @@
+// task_5/dashboard/src/actions/notificationActionCreators.js
+
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+
+export const markAsRead = (index) => {
+  return {
+    type: MARK_AS_READ,
+    index,
+  };
+};
+
+export const setNotificationFilter = (filter) => {
+  return {
+    type: SET_TYPE_FILTER,
+    filter,
+  };
+};
+
