259c1ecd9c302f04bf9d48f0876bcb15c8417f09
Makoena20 <koenaletseku@gmail.com>
2024-10-02 13:07:19 +0000

    task_3

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,18 @@
+// task_3/dashboard/src/actions/courseActionCreators.js
+
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+export const selectCourse = (index) => {
+  return {
+    type: SELECT_COURSE,
+    index,
+  };
+};
+
+export const unSelectCourse = (index) => {
+  return {
+    type: UNSELECT_COURSE,
+    index,
+  };
+};
+
